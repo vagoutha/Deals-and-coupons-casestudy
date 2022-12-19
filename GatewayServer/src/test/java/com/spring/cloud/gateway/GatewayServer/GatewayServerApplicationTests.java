7ce9c3b53bfd9bad6bbe3ad3a80e0dce46af3d8e
vagoutha <115881588+vagoutha@users.noreply.github.com>
2022-12-19 10:18:29 +0530

    Add files via upload

---
+++ GatewayServer/src/test/java/com/spring/cloud/gateway/GatewayServer/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.gateway.GatewayServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
